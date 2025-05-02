7e375c048c16569de41971cfa4ebe66380d935d4
kaeseNachos <max.hartert@it-top.de>
2025-05-03 00:40:27 +0200

    cleanup

--- keyboards/bastardkb/charybdis/4x6/keymaps/custom/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/custom/keymap.c
@@ -56,9 +56,8 @@ enum charybdis_keymap_layers {
 
  td_state_t cur_dance(tap_dance_state_t *state);
  
- // For the x tap dance. Put it here so it can be used in any keymap
- void x_finished(tap_dance_state_t *state, void *user_data);
- void x_reset(tap_dance_state_t *state, void *user_data);
+ void lnt_finished(tap_dance_state_t *state, void *user_data);
+ void lnt_reset(tap_dance_state_t *state, void *user_data);
 
 /** \brief Automatically enable sniping-mode on the pointer layer. */
 #define CHARYBDIS_AUTO_SNIPING_ON_LAYER LAYER_POINTER
@@ -210,20 +209,13 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
    td_state_t cur_dance(tap_dance_state_t *state) {
      if (state->count == 1) {
          if (state->interrupted || !state->pressed) return TD_SINGLE_TAP;
-         // Key has not been interrupted, but the key is still held. Means you want to send a 'HOLD'.
          else return TD_SINGLE_HOLD;
      } else if (state->count == 2) {
-         // TD_DOUBLE_SINGLE_TAP is to distinguish between typing "pepper", and actually wanting a double tap
-         // action when hitting 'pp'. Suggested use case for this return value is when you want to send two
-         // keystrokes of the key, and not the 'double tap' action/macro.
          if (state->interrupted) return TD_DOUBLE_SINGLE_TAP;
          else if (state->pressed) return TD_DOUBLE_HOLD;
          else return TD_DOUBLE_TAP;
      }
  
-     // Assumes no one is trying to type the same letter three times (at least not quickly).
-     // If your tap dance key is 'KC_W', and you want to type "www." quickly - then you will need to add
-     // an exception here to return a 'TD_TRIPLE_SINGLE_TAP', and define that enum just like 'TD_DOUBLE_SINGLE_TAP'
      if (state->count == 3) {
          if (state->interrupted || !state->pressed) return TD_TRIPLE_TAP;
          else return TD_TRIPLE_HOLD;
@@ -231,14 +223,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
  }
  
  // Create an instance of 'td_tap_t' for the 'x' tap dance.
- static td_tap_t xtap_state = {
+ static td_tap_t lnttap_state = {
      .is_press_action = true,
      .state = TD_NONE
  };
  
  void lnt_finished(tap_dance_state_t *state, void *user_data) {
-     xtap_state.state = cur_dance(state);
-     switch (xtap_state.state) {
+     lnttap_state.state = cur_dance(state);
+     switch (lnttap_state.state) {
          case TD_SINGLE_TAP: layer_on(LAYER_LOWER); break;
          case TD_SINGLE_HOLD: layer_on(LAYER_LOWER); break;
          case TD_DOUBLE_TAP: register_code(KC_LCTL); tap_code(KC_T);break;
@@ -248,14 +240,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
  }
  
  void lnt_reset(tap_dance_state_t *state, void *user_data) {
-     switch (xtap_state.state) {
+     switch (lnttap_state.state) {
          case TD_SINGLE_TAP: layer_off(LAYER_LOWER); break;
          case TD_SINGLE_HOLD: layer_off(LAYER_LOWER); break;
          case TD_DOUBLE_TAP: unregister_code(KC_LCTL); break;
          case TD_DOUBLE_HOLD: layer_off(LAYER_LOWER); break;
          default: break;
      }
-     xtap_state.state = TD_NONE;
+     lnttap_state.state = TD_NONE;
  }
 
 
